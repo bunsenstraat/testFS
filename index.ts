3adc5aee266c19a2a219b1a481a1d25036a66d8b
filip mertens <filip.mertens@ethereum.org>
2023-12-18 15:51:07 +0100

    smaller

--- index.ts
+++ index.ts
@@ -1,11 +1,15 @@
 import fs from "fs";
 
 // create 10000 directories in test directory and put a text file in each directory
-for (let i = 0; i < 10000; i++) {
+for (let i = 0; i < 500; i++) {
   try {
-    fs.mkdirSync(`./test/${i}`);
-  } catch (e) {}
+    fs.mkdirSync(`./test`);
+  } catch (e) {
+
+  }
   try {
-    fs.writeFileSync(`./test/${i}/test.txt`, "Hello World");
-  } catch (e) {}
+    fs.writeFileSync(`./test/test${i}.txt`, "Hello World");
+  } catch (e) {
+    console.log(e);
+  }
 }
